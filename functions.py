9591d85dac9fb667726257796bf48cb82f371e14
Apollo <nobitagain@gmail.com>
2024-09-15 09:54:36 -0300

    2° Update do Campo Minado
     - Jogo praticamente pronto
    - Falta correção de bugs

--- functions.py
+++ functions.py
@@ -13,9 +13,30 @@ def criarMapa(arquivo):
     mapa = []
     for linha in arquivo.readlines()[0:tamanho]:
         mapa.append([int(x) for x in linha.strip().split(" ")])
-    return mapa
+    return np.array(mapa), tamanho
 
-# Implementar do jeito da Bea
-def exibirMapa(mapa):
-    for i in mapa:
+def mapaUsuario(tamanho):
+    matriz = np.full((tamanho, tamanho), "#")
+    return matriz
+
+def exibirMapa(matriz):
+    for i in matriz:
         print(i)
+
+def revelarEspaco(mapa, linha, coluna):
+    if(mapa[linha][coluna] == 1):
+        return False, "X"
+    else:
+        simbolo = verificarEspaco(mapa, linha, coluna)
+        return True, simbolo
+    
+def verificarEspaco(mapa, linha, coluna):
+    contador = 0
+    array = [mapa[linha-1][coluna-1], mapa[linha-1][coluna], mapa[linha-1][coluna+1], mapa[linha][coluna-1], mapa[linha][coluna+1], mapa[linha+1][coluna-1], mapa[linha+1][coluna], mapa[linha+1][coluna+1]]
+    for espaco in array:
+        if(espaco == 1):
+            contador+=1
+    if(contador > 0):
+        return contador
+    else:
+        return "-"
--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import numpy as np
 arrayMapas = func.criarArrayMapas(3)
 mapaEscolhido = func.mapaAleatorio(arrayMapas)
 arquivo = open(f"./mapas/{str(mapaEscolhido)}.txt", "r")
+mapa, tamanho = func.criarMapa(arquivo)
+matriz = func.mapaUsuario(tamanho)
 
-mapa = func.criarMapa(arquivo)
+run = True
 
-func.exibirMapa(mapa)
-
-#for row in mapa:
-#    print(' '.join(map(str,row)))
+while(run):
+    func.exibirMapa(matriz)
+    linha = int(input("Insira a linha do espaço que deseja abrir: "))
+    coluna = int(input("Insira a coluna do espaço que deseja abrir: "))
+    linha-=1
+    coluna-=1
+    run, matriz[linha][coluna] = func.revelarEspaco(mapa, linha, coluna)
+print("Game Over")
